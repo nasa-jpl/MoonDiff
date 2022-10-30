64952b3099180431445331ebe0541aa1312e4aa3
Aaron Curtis <foobarbecue@gmail.com>
2022-10-29 19:08:22 -0700

    register models in admin

--- moondiff/core/admin.py
+++ moondiff/core/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from moondiff.core.models import SpacecraftCamera, Image, PairSet, Pair, Annotation
+
+admin.site.site_header = "MoonDiff admin"
+admin.site.site_title = "MoonDiff admin"
+
+admin.site.register(SpacecraftCamera)
+admin.site.register(Image)
+admin.site.register(PairSet)
+admin.site.register(Pair)
+admin.site.register(Annotation)
 
 # Register your models here.
--- moondiff/core/models.py
+++ moondiff/core/models.py
@@ -2,6 +2,9 @@ from django.contrib.gis.db import models
 
 class SpacecraftCamera(models.Model):
     name = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.name
 
 
 class Image(models.Model):
@@ -9,17 +12,24 @@ class Image(models.Model):
     product_id = models.CharField(max_length=100)
     file_data = models.FileField()
 
+    def __str__(self):
+        return self.product_id
 
 class PairSet(models.Model):
     name = models.CharField(max_length=100)
     notes = models.TextField(blank=True)
-    pass
+    
+    def __str__(self):
+        return self.name
 
 
 class Pair(models.Model):
     old_image = models.ForeignKey(Image, on_delete=models.CASCADE, related_name='old_image')
     new_image = models.ForeignKey(Image, on_delete=models.CASCADE, related_name='new_image')
     pairset = models.ForeignKey(PairSet, on_delete=models.CASCADE, blank=True)
+
+    def __str__(self):
+        return f"{self.old_image}xx{self.new_image}"
 
     class Meta:
         unique_together = ['old_image', 'new_image']
