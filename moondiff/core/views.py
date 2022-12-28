50b4836dddbb55e035a812624a75e92d4a3764ad
Aaron Curtis <foobarbecue@gmail.com>
2022-12-28 15:21:20 -0800

    Make detections display on review page, misc.

--- moondiff/core/views.py
+++ moondiff/core/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import DetailView
+from django.views.generic import DetailView, RedirectView
 from moondiff.core.models import Pair, Annotation, AnnotationReview, get_random
 from moondiff.core.serializers import AnnotationSerializer, AnnotationForPairSerializer, ReviewFormSerializer, SubmitReviewSerializer
 from rest_framework import viewsets, permissions
@@ -7,6 +7,7 @@ from rest_framework.views import APIView
 from rest_framework.renderers import TemplateHTMLRenderer
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
+from django.urls import reverse
 
 @method_decorator(login_required, name='dispatch')
 class PairDetailView(DetailView):
@@ -45,30 +46,25 @@ class AddReviewView(APIView):
     model = AnnotationReview
     renderer_classes = [TemplateHTMLRenderer]
     template_name = 'core/annotation_review.html'
-    
+
     #TODO should really have custom user model designating reviewers
     permission_classes = [permissions.IsAdminUser]
-    
+
     # Get an annotation to review 
     def get(self, request, *args, **kwargs):
-        
+
         # Find an unreviewed annotation and return it
-        annotation = get_random(Annotation)
-        
-        # Remove the related annotation field from ReviewSerializer. We don't
-        # want it as a combobox in the form because we already selected it --
-        # it's in hidden input in the template.
-        
-        
+        annotation = Annotation.objects.get(pk=kwargs['pk'])
+
         context = {
             'review_serializer': ReviewFormSerializer,
             'annotation': annotation,
             'pair': annotation.pair
         }
-        
+
         return Response(context)
 
-    def post(self, request):
+    def post(self, request, *args, **kwargs):
         review_serialized = SubmitReviewSerializer(data=request.data)
         if review_serialized.is_valid():
             review_serialized.save(
@@ -76,3 +72,21 @@ class AddReviewView(APIView):
             )
             # TODO bug: after a post, the page shown uses the wrong serializer
         return Response({'message': review_serialized.errors, 'review_serializer': SubmitReviewSerializer})
+
+class SelectPairView(RedirectView):
+    """
+    This is a view that selects a random detection to review, and returns a
+    redirect to the page for reviewing that detection.
+    """
+    def get_redirect_url(self, *args, **kwargs):
+        pair = get_random(Pair)
+        return pair.get_absolute_url()
+
+class SelectReviewView(RedirectView):
+    """
+    This is a view that selects a random detection to review, and returns a
+    redirect to the page for reviewing that detection.
+    """
+    def get_redirect_url(self, *args, **kwargs):
+        annotation = get_random(Annotation)
+        return reverse('review-detection', kwargs={'pk':annotation.pk})
--- moondiff/urls.py
+++ moondiff/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path, re_path
-from moondiff.core.views import PairDetailView, AnnotationViewSetThisUser, AddReviewView
+from moondiff.core.views import PairDetailView, AnnotationViewSetThisUser, \
+    AddReviewView, SelectReviewView, SelectPairView
 from django.views.generic import TemplateView, RedirectView
 from django.contrib.auth.decorators import login_required
 from django.conf.urls.static import static
@@ -29,8 +30,10 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('admin/login/', RedirectView.as_view(pattern_name='account_login', permanent=True)),
     path('admin/', admin.site.urls),
-    re_path('pair/(?P<pk>[a-z0-9]+)', PairDetailView.as_view(), name='pair-detail'),
-    re_path('review/', AddReviewView.as_view(), name='review-detections'),
+    re_path('compare_pair/select', SelectPairView.as_view(), name='select-pair-to-compare'),
+    re_path('compare_pair/(?P<pk>[a-z0-9]+)', PairDetailView.as_view(), name='pair-detail'),
+    re_path('review_detection/select', SelectReviewView.as_view(), name='select-detection-to-review'),
+    re_path('review_detection/(?P<pk>[a-z0-9]+)', AddReviewView.as_view(), name='review-detection'),
     path('api/', include(router.urls)), # API urls
     path('', TemplateView.as_view(template_name='frontpage.html'), name='frontpage')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
