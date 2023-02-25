0f2bb7e12353563c00a0aa07404182555d142f93
aacurtis <aaron.curtis@jpl.nasa.gov>
2023-02-25 03:36:07 -0800

    Fix delete buttons

--- static/detections.js
+++ static/detections.js
@@ -1,24 +1,27 @@
 const detections = document.getElementById("detections");
 const detectionsTitle = document.getElementById("detections-title");
-const detectionDelButtons = document.querySelector('.detection-del-button')
-detectionsTitle.addEventListener('click', (evt) => {
+const detectionDelButtons = document.querySelectorAll('.detection-del-button')
+detectionsTitle.addEventListener('click', () => {
     detections.classList.toggle('collapsed');
 });
 
-detectionDelButtons.addEventListener('click',(evt)=>{
+for (delButton of detectionDelButtons){
+    delButton.addEventListener('click',(evt)=>{
     const detectionUrl = evt.target.dataset.detectionUrl;
     fetch(detectionUrl,{
-        method: 'DELETE',
-        credentials: 'same-origin',
-        headers: {'x-csrftoken': _csrfToken}
+            method: 'DELETE',
+            credentials: 'same-origin',
+            headers: {'x-csrftoken': _csrfToken}
         }
     ).then(
         (response) => {
             if (response.ok) {
                 evt.target.parentElement.remove()
+                window.location.reload()
             } else {
                 alert('There was a problem deleting your change detection.');
             }
         }
     );
 })
+}
