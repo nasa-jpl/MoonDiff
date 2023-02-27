1ec33ed8edc6e7e72a6c17bd51debf4f1f0d0235
aacurtis <aaron.curtis@jpl.nasa.gov>
2023-02-26 17:36:14 -0800

    Add image start time

--- moondiff/core/models.py
+++ moondiff/core/models.py
@@ -26,7 +26,7 @@ class Image(models.Model):
                                           on_delete=models.CASCADE)
     product_id = models.CharField(max_length=100)
     file_data = models.FileField(upload_to='images/')
-
+    start_time = models.DateTimeField(blank=True, null=True)
     def __str__(self):
         return self.product_id
 
