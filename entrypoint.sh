034efb877b524d2f04897311e57b799184e35c96
aacurtis <aaron.curtis@jpl.nasa.gov>
2023-07-02 14:55:05 -0700

    bump version, add example fixture to entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,6 +11,7 @@ then
   python manage.py loaddata apollo14.yaml
   python manage.py loaddata apollo12.yaml
   python manage.py loaddata alphonsus.yaml
+  python manage.py loaddata examples.yaml
 fi
 python manage.py collectstatic --noinput
 gunicorn moondiff.wsgi:application --bind 0.0.0.0:8000
