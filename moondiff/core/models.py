a28de700b278406ffaa51141a329cbb5cec7689a
aacurtis <aaron.curtis@jpl.nasa.gov>
2023-07-04 16:00:08 -0700

    prevent div by 0 (attempt 2, sigh)

--- moondiff/core/models.py
+++ moondiff/core/models.py
@@ -11,7 +11,7 @@ class MoonDiffUser(AbstractUser):
     @property
     def avg_visit_duration(self):
         visit_durations = [visit.duration for visit in self.visit_set.all()]
-        if len(visit_durations > 0):
+        if len(visit_durations) > 0:
             return sum(visit_durations, datetime.timedelta()).total_seconds() / len(visit_durations)
         else:
             return 0
