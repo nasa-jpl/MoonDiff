fe99afbe90e20135edd649c89bd6f7703743dd45
aacurtis <aaron.curtis@jpl.nasa.gov>
2023-07-11 21:08:24 -0700

    show explanations on examples page

--- static/examples.js
+++ static/examples.js
@@ -3,3 +3,9 @@ const examplesTitle = document.getElementById("examples-title");
 examplesTitle.addEventListener("click", () => {
   examples.classList.toggle("collapsed");
 });
+
+const exampleNotes = document.getElementById("example-notes");
+const examplesNotesTitle = document.getElementById("example-notes-title");
+examplesNotesTitle.addEventListener("click", () => {
+  exampleNotes.classList.toggle("collapsed");
+});
